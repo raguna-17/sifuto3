61c6817cec070b8345839a54f5e6985a463e7962
らぐな <pasupoto17@gmail.com>
2026-05-09 17:38:00 +0900

    Initial commit

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,5 @@
-from pydantic_settings import BaseSettings,SettingsConfigDict,Field
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from pydantic import Field
 
 
 class Settings(BaseSettings):
