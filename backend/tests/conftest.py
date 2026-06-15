5d4e862cabd30f1ea19a788d3e79c44e8a943561
らぐな <pasupoto17@gmail.com>
2026-06-15 14:41:35 +0900

    Initial commit

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-
+from app.domains.users.model import User
 from app.main import app
 from app.db.session import get_db
 
